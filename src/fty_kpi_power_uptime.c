921ee0558f30c972a7bf0bd7a77cb1119d65a6b4
bstepankova <bstepankova@volny.cz>
2017-06-06 16:06:39 +0200

    Problem: regexp stopped working for some reason
    Solution: setting done separately
    
    Signed-off-by: Barbora Stepankova <BarboraStepankova@eaton.com>

--- src/fty_kpi_power_uptime.c
+++ src/fty_kpi_power_uptime.c
@@ -1,21 +1,21 @@
 /*  =========================================================================
     fty_kpi_power_uptime - Main daemon
 
-    Copyright (C) 2014 - 2017 Eaton                                        
-                                                                           
-    This program is free software; you can redistribute it and/or modify   
-    it under the terms of the GNU General Public License as published by   
-    the Free Software Foundation; either version 2 of the License, or      
-    (at your option) any later version.                                    
-                                                                           
-    This program is distributed in the hope that it will be useful,        
-    but WITHOUT ANY WARRANTY; without even the implied warranty of         
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          
-    GNU General Public License for more details.                           
-                                                                           
+    Copyright (C) 2014 - 2017 Eaton
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
     You should have received a copy of the GNU General Public License along
     with this program; if not, write to the Free Software Foundation, Inc.,
-    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.            
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
     =========================================================================
 */
 
@@ -65,7 +65,8 @@ int main (int argc, char *argv [])
     zsock_wait (server);
     zstr_sendx (server, "CONNECT", endpoint, NULL);
     zsock_wait (server);
-    zstr_sendx (server, "CONSUMER", "METRICS", "^(status.ups|status)@.*", NULL);
+    zstr_sendx (server, "CONSUMER", "METRICS", "^status.ups@.*", NULL);
+    zstr_sendx (server, "CONSUMER", "METRICS", "^status@.*", NULL);
     zstr_sendx (server, "CONSUMER", "ASSETS", "^datacenter.unknown@.*", NULL);
     zsock_wait (server);
 
